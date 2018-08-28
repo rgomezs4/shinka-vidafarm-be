984c6962abf830a4fac5bd9661a66102fc00b094
Rodrigo Gomez <rgomez@corporacionmastergroup.com>
2018-08-28 09:54:57 -0600

    delete invoice files

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
-import invoice from "./invoice/invoice";
 import shows from "./shows/shows";
 
 module.exports = app => {
     app.use("/api/v1", shows);
-    app.use("/api/v1", invoice);
 } /* end of module */
